70e78accece4cc8cdf50120b81af0e41048aeded
b7c8p <morapaezc@gmail.com>
2022-01-20 20:41:42 -0500

    fixes mobile

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,11 +5,11 @@ export const Search = ({ searchCity, setSearchCity }) => {
     setSearchCity(e.target.value)
   }
   return (
-    <label className='relative block'>
+    <label className='inline-block'>
       <input
         value={searchCity}
         onChange={handleCity}
-        className='text-2xl placeholder:italic placeholder:text-gray-400 bg-inherit block border-b-2 border-gray-900 py-2 pl-3 focus:outline-none dark:text-white dark:border-b dark:border-white' type="text" placeholder='Busca un País' />
+        className='text-xl sm:text-2xl placeholder:italic placeholder:text-gray-400 bg-inherit block border-b-2 border-gray-900 py-2 pl-3 focus:outline-none dark:text-white dark:border-b dark:border-white' type="text" placeholder='Busca un País' />
     </label>
   )
 }
--- src/container/SearchCountries.jsx
+++ src/container/SearchCountries.jsx
@@ -1,6 +1,6 @@
 export const SearchCountries = ({ children }) => {
   return (
-    <section className="grid grid-rows-2 gap-5 w-3/4 mx-auto place-items-center my-8 sm:grid-cols-2 sm:grid-rows-1 sm:w-5/6">
+    <section className="grid grid-rows-2 gap-5 w-3/4 mx-auto  justify-items-center content-center my-8 sm:grid-cols-2 sm:grid-rows-1 sm:w-5/6">
       {children}
     </section>
   )
